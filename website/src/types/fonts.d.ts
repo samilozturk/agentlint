9bee84ca04bad4d8b73e4bf7b741024649a7f68d
Bedrettin Şamil Öztürk <bsamilozturk@gmail.com>
2026-04-02 22:41:19 +0300

    fix(website): add @fontsource-variable/inter module declaration to fonts.d.ts

--- website/src/types/fonts.d.ts
+++ website/src/types/fonts.d.ts
@@ -1,5 +1,2 @@
+declare module "@fontsource-variable/inter";
 declare module "@fontsource-variable/jetbrains-mono";
-declare module "@fontsource/inter/400.css";
-declare module "@fontsource/inter/500.css";
-declare module "@fontsource/inter/600.css";
-declare module "@fontsource/inter/700.css";
